faf7be7908219b7930d4cec148a5d148f9124391
jyjang <jyjang@uengine.org>
2019-02-15 13:41:42 +0900

    change Dockerfile to have build stage

--- src/main/java/com/uengine/education/MailLogRepository.java
+++ src/main/java/com/uengine/education/MailLogRepository.java
@@ -1,10 +1,12 @@
 package com.uengine.education;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * Created by uengine on 2019. 2. 13..
  */
+@RepositoryRestResource(collectionResourceRel = "v1/mailLogs", path = "v1/mailLogs")
 public interface MailLogRepository extends PagingAndSortingRepository<MailLog, Long>{
 
 }
