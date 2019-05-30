f8b5105b9cf8fab2ad8e76461108091dff3ced93
jyjang <jyjang@uengine.org>
2019-05-30 16:03:39 +0900

    version 2 update

---
+++ src/main/java/com/uengine/education/ClazzDayRegistered.java
@@ -0,0 +1,29 @@
+package com.uengine.education;
+
+/**
+ * Created by uengine on 2019. 4. 19..
+ */
+public class ClazzDayRegistered {
+
+    long date;
+    String title;
+    String type;
+
+    public long getDate() {
+        return date;
+    }
+
+    public void setDate(long date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+}
--- src/main/java/com/uengine/education/EventListener.java
+++ src/main/java/com/uengine/education/EventListener.java
@@ -12,24 +12,36 @@ public class EventListener {
     @Autowired
     MailLogRepository mailLogRepository;
 
+//    @StreamListener(Streams.INPUT)
+//    @JsonDeserialize(as = Enrolled.class)
+//    public void handleEnrolled(@Payload Enrolled enrolled) {
+//
+//        String title = "제목: " + enrolled.getCustomerName() +" 님의 강의 수강이 신청되었습니다.";
+//        String contents = "수강신청과목명: " + enrolled.getCourseTitle();
+//
+//        MailLog mailLog = new MailLog();
+//        mailLog.setTo(enrolled.getCustomerId());
+//        mailLog.setContents(contents);
+//        mailLog.setTitle(title);
+//
+//        mailLogRepository.save(mailLog);
+//
+//        System.out.println("<이메일 발송>");
+//        System.out.println(title);
+//        System.out.println(contents);
+//
+//    }
+
     @StreamListener(Streams.INPUT)
-    @JsonDeserialize(as = Enrolled.class)
-    public void handleEnrolled(@Payload Enrolled enrolled) {
+    @JsonDeserialize(as = ClazzDayRegistered.class)
+    public void handleClazzDay(@Payload ClazzDayRegistered clazzDayRegistered) {
 
-        String title = "제목: " + enrolled.getCustomerName() +" 님의 강의 수강이 신청되었습니다.";
-        String contents = "수강신청과목명: " + enrolled.getCourseTitle();
+        clazzDayRegistered.
 
-        MailLog mailLog = new MailLog();
-        mailLog.setTo(enrolled.getCustomerId());
-        mailLog.setContents(contents);
-        mailLog.setTitle(title);
-
-        mailLogRepository.save(mailLog);
-
-        System.out.println("<이메일 발송>");
-        System.out.println(title);
-        System.out.println(contents);
-
+        System.out.println("제목 : [마감임박] "+ clazzDayRegistered.getTitle() + " 강의가 개설되었습니다");
+        System.out.println("내용 : 일시: "+ clazzDayRegistered.getDate());
+        System.out.println("*** 기념품 증정 ***: ");
     }
+
 
 }
